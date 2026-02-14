0f6abbe53c5c8db21857e84e5d4bedfe1fbb77ec
ddeepak95 <ddeepak95@gmail.com>
2026-02-14 14:34:58 -0500

    style update

--- src/components/Shared/PageTitle.tsx
+++ src/components/Shared/PageTitle.tsx
@@ -9,9 +9,14 @@ interface PageTitleProps {
 
 export default function PageTitle({ title, badge, className }: PageTitleProps) {
   return (
-    <div className={cn("flex flex-col gap-2", className)}>
+    <div
+      className={cn(
+        "flex flex-col gap-2 border-b border-gray-500 pb-2",
+        className,
+      )}
+    >
       {badge}
-      <h1 className="text-3xl font-bold">{title}</h1>
+      <h1 className="text-xl">{title}</h1>
     </div>
   );
 }
