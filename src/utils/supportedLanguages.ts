d42b50f0c3c58f6844ef8db2a9830f37b72aa66f
ddeepak95 <ddeepak95@gmail.com>
2026-03-25 13:37:48 -0400

    bengali removed

--- src/utils/supportedLanguages.ts
+++ src/utils/supportedLanguages.ts
@@ -1,8 +1,7 @@
 export const supportedLanguages = [
     {code: "en", name: "English" },
     {code: "en-IN", name: "English (India)"},
-    {code:"hi", name: "Hindi"},
-    {code:"bn", name: "Bengali"},
+    {code:"hi", name: "Hindi"},,
     {code: "ta", name: "Tamil"},
     {code:"de", name: "German"},
     {code:"kn", name: "Kannada"},
